6159ebe66bcd62ed07ed2dcafb844eb8e75fd2cb
Aziz Chharchhodawala <172964858+aziz-ch52@users.noreply.github.com>
2026-03-29 10:30:17 +0530

    Update day22.py

--- day22.py
+++ day22.py
@@ -1,17 +1,17 @@
 
-# Program to Print a Right-Angled Triangle of '*'
+# Program to Print a Right-Angled Triangle of '*.'
 
 # Step 1: Define the height of the triangle
 rows = 5
 
-# Step 2: Use outer loop for each row
+# Step 2: Use an outer loop for each row
 for i in range(1, rows + 1):
 
     # Step 3: Inner loop prints stars equal to current row number
     for j in range(i):
         print("*", end=" ")
 
-    # Step 4: Move to next line after printing stars in the row
+    # Step 4: Move to the next line after printing stars in the row
     print()
 
 # End of Program
